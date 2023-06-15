d8c535984d8ebd240311d647a89b2dd0e6046a90
Miche <valemiche003@gmail.com>
2023-06-15 13:48:12 -0300

    Commit de la API avanzada

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,30 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lifeyears: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,14 @@
-const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
-
-const router = Router();
-
+const router = require('express').Router();
+const getDogs = require('../controllers/getDogs')
+const getDogs = require('../controllers/getDogsById')
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.get('/dogs', getDogs);
+router.get('/dogs/idRaza', getDogById)
+router.get('/dogs/name?=', getDogByName)
+router.post('/dogs', postDogs);
+router.get('/temperaments', getTemperaments);
 
 module.exports = router;
